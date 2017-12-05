47aaf8638720ec5beafa89f71a669af5f05ad538
colbyt093 <colbyt.093@gmail.com>
2017-12-05 01:15:37 +0000

    asdf

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -14,14 +14,18 @@ export class Navbar extends React.Component{
                   <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav mr-auto">
                       <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        <a className="nav-link" href="/categories">Categories<span className="sr-only">(current)</span></a>
                       </li>
                       <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="/bestsellers">Best Sellers</a>
+                      </li>
+                      <li className="cart">
+                        <a className="cart-link" href="/cart">
+                        <span class="glyphicon glyphicon-shopping-cart"></span></a>
                       </li>
                       <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          Dropdown
+                          Dropdown Here
                         </a>
                         <div className="dropdown-menu" aria-labelledby="navbarDropdown">
                           <a className="dropdown-item" href="#">Action</a>
