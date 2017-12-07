ebfd54e82ea761e54feb779611fd9ad0e58a3736
Carlos <caenavgu@gmail.com>
2017-12-07 00:14:48 +0000

    asdf

--- src/js/components/Searchbar.jsx
+++ src/js/components/Searchbar.jsx
@@ -5,14 +5,14 @@ export class Searchbar extends React.Component{
     render(){
         
         return(
-               <nav className="navbar bg-light ">
-  <div className="media">
-    <img className="m" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Yin_yang.svg/1200px-Yin_yang.svg.png" alt="Generic placeholder image" height="42" width="42" />
-  </div>
-  <input className="form-control col-5" type="search" placeholder="Search" aria-label="Search" />
-  <button className="btn btn-success" type="submit"><i className="fa fa-search" aria-hidden="true"></i></button>
-  <button className="btn btn-success" id='c' type="submit">LOGIN</button>
-  </nav>
+            <nav className="navbar bg-light ">
+                <div className="media">
+                    <img className="m" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Yin_yang.svg/1200px-Yin_yang.svg.png" alt="Generic placeholder image" height="42" width="42" />
+                </div>
+                <input className="form-control col-5" type="search" placeholder="Search" aria-label="Search" />
+                <button className="btn btn-success" type="submit"><i className="fa fa-search" aria-hidden="true"></i></button>
+                <button className="btn btn-success" id='c' type="submit">LOGIN</button>
+            </nav>
             );  
     }
     
