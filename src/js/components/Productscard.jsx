5c024538f5500fe9f6757f45d91ad8db9de8e06f
colbyt093 <colbyt.093@gmail.com>
2017-12-11 13:38:54 +0000

    updated w/ new images

--- src/js/components/Productscard.jsx
+++ src/js/components/Productscard.jsx
@@ -15,19 +15,19 @@ export class Productscard extends React.Component{
     </div>
   </div>
   <div className="card">
-    <img className="card-img-top" src="/src/img/homeProducts/product-shirt-panda.png" alt="Card image cap" />
+    <img className="card-img-top" src="/src/img/homeProducts/product-shirt-bsod.png" alt="Card image cap" />
     <div className="card-block">
-      <h4 className="card-title">HOT AF PANDA SHIRT 2</h4>
-      <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      <h4 className="card-title">Blue Shirt of Death</h4>
+      <p className="card-text">This shirt is only available in blue.</p>
+      <p className="card-text"><small className="text-muted">Limited run!</small></p>
     </div>
   </div>
   <div className="card">
-    <img className="card-img-top" src="/src/img/homeProducts/product-shirt-panda.png" alt="Card image cap" />
+    <img className="card-img-top" src="/src/img/homeProducts/product-shirt-password.png" alt="Card image cap" />
     <div className="card-block">
-      <h4 className="card-title">HOT AF PANDA SHIRT 3</h4>
-      <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+      <h4 className="card-title">Your Mom's Password...</h4>
+      <p className="card-text">No dev shirt before or since has started so many fights. Great way to get the night started!</p>
+      <p className="card-text"><small className="text-muted">Come at me, dev.</small></p>
     </div>
   </div>
 </div>
