b60988cff8908a524a2516615055ac94f49971f2
colbyt093 <colbyt.093@gmail.com>
2018-02-06 02:22:15 +0000

    update link tags

--- src/js/Layout.jsx
+++ src/js/Layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import {Home} from './Home';
 import {Profile} from './Profile1';
-import {NewUser} from './NewUser';
+import {NewUser} from './newuser';
 import {Product} from './Product';
 
 export class Layout extends React.Component{
@@ -16,7 +16,7 @@ export class Layout extends React.Component{
                         <Route exact path='/index.html' component={Home} />
                         <Route exact path='/' component={Home} />                        
                         <Route exact path='/profile' component={Profile} />
-                        <Route exact path='/NewUser' component={NewUser} />
+                        <Route exact path='/newuser' component={NewUser} />
                         <Route exact path='/Product' component={Product} />
                     </div>
                 </BrowserRouter> 
--- src/js/components/Searchbar.jsx
+++ src/js/components/Searchbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 export class Searchbar extends React.Component{
     
@@ -19,33 +20,37 @@ export class Searchbar extends React.Component{
                     <button className="btn btn-success col-2" type="button"><i className="fa fa-search" aria-hidden="true"></i></button>
             </div>
             </div>*/}
-            
-            <form class="form-inline">
+
+            <form className="form-inline justify-content-center">
   {/*<div class="form-group mb-2">
     <label for="staticEmail2" class="sr-only">Email</label>
     <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="email@example.com" />
   </div>*/}
-  <div class="form-group mx-sm-8 mb-2">
-    <label for="inputPassword2" class="sr-only">Password</label>
-    <input type="password" class="form-control" id="inputPassword2" placeholder="Find something awesome here!" />
+  <div className="form-group mx-sm-10 mb-4">
+    <label for="inputPassword2" className="sr-only">Password</label>
+    <input type="password" className="form-control" id="inputPassword2" placeholder="Find something awesome here!" />
   </div>
-  <button type="submit" class="btn btn-primary mb-2"><i className="fa fa-search" aria-hidden="true"></i></button>
+  <button type="submit" className="btn btn-primary mb-4"><i className="fa fa-search" aria-hidden="true"></i></button>
 </form>
-            
-            
-            
-            <div class="container">
-            <div class="span12">
-                            <i class="fas fa-shopping-cart"></i>
-                  </div>
-                  </div>
-                  
 
-                  {/*<div className="col-2">
+            
+            
+            {/*
+                <div class="container">
+                <div class="span12">
+                                <i class="fas fa-shopping-cart"></i>
+                      </div>
+                      </div>
+
+            */}      
+
+             
+                      <div>
         
                         <button className="btn btn-outline-info pull-right" id='c' type="submit"><a href="/Profile">LOGIN</a></button><br />
-                        <a class="btn pull-right" href="/NewUser">New? Start here.</a>
-                  </div>*/}
+                        <Link class="btn pull-right" to="/newuser">New? Start here.</Link>
+                  </div>
+              
                           
             </div>
             );  
