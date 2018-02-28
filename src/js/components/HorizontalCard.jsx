ebb2e1dd598178c8c12a7f2ac0e7c1e3d14fd368
colbyt093 <colbyt.093@gmail.com>
2018-02-28 23:38:25 +0000

    product upload

--- src/js/components/HorizontalCard.jsx
+++ src/js/components/HorizontalCard.jsx
@@ -11,9 +11,9 @@ export class HorizontalCard extends React.Component{
         <div className="col-md-4">
             <img src="/src/img/homeProducts/devshui-help.jpg" className="w-100"/>
           </div>
-          <div className="col-md-8 px-3">
-            <div className="card-block px-3">
-              <h4 className="card-title">Dear Miami: WE LOVE YOU</h4>
+          <div className="col-md-8 p-5">
+            <div className="card-block">
+              <h4 className="card-title">We Love MIA, So We Dev MIA</h4>
               <p className="card-text">Did we come off too strong? Hopefully not- each order that you make contributes to the lesser priveleged to learn the skills they need to help contribute to Miami's diverse, flourishing tech landscape.</p>
               <p className="card-text">If you know someone that has the drive to learn, you can nominate them to receive a partial or full scholarship to a certified local coding academy.</p>
               <a href="#" className="btn btn-primary">Read More</a>
