3fae0842d62976a8b70c866ceff25129c273acb0
colbyt093 <colbyt.093@gmail.com>
2018-03-02 13:09:38 +0000

    home page design

--- src/js/components/Promotion.jsx
+++ src/js/components/Promotion.jsx
@@ -5,11 +5,11 @@ export class Promotion extends React.Component{
     render(){
         
         return(
-           <div className='container-fluid bg-dark'>
+           <div className='container-fluid'>
                 <div className="row">
                     <div className="col-xs-6 mx-auto">
-                        <img id='promotion' src="https://i.imgur.com/r2Rcbp5.jpg" className="rounded" alt="..."/>
-                        <img id='promotion' src="https://i.imgur.com/XirOjZZ.jpg" className="rounded" alt="..."/>
+                        <img id='promotion' src="https://i.imgur.com/r2Rcbp5.jpg" className="rounded pl-5" alt="..."/>
+                        <img id='promotion' src="https://i.imgur.com/XirOjZZ.jpg" className="rounded pl-5" alt="..."/>
                     </div>
                 </div>
            </div>
